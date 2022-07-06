b12362784de259aa9a8c6818341a952760dadfd1
MonicaQuezada <you@example.com>
2022-07-05 21:51:05 -0500

    "Eureka"

--- service-shopping/service-shopping/src/main/java/ucuenca/ejemplo/serviceshopping/ServiceShoppingApplication.java
+++ service-shopping/service-shopping/src/main/java/ucuenca/ejemplo/serviceshopping/ServiceShoppingApplication.java
@@ -2,8 +2,10 @@ package ucuenca.ejemplo.serviceshopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceShoppingApplication {
 
 	public static void main(String[] args) {
